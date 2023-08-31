edee2d6c861d567117d085cdc3f3384b4c0599c9
Alexander Granin <alexander@genlambda.com>
2023-08-31 19:39:29 +0800

    Ch2 sample improved

--- First-Edition/BookSamples/CH02/Section2p2/src/Main.hs
+++ First-Edition/BookSamples/CH02/Section2p2/src/Main.hs
@@ -1,13 +1,16 @@
+{-# LANGUAGE TypeApplications #-}
 module Main where
 
-import Board (Board)
-import Cell (Cell(..))
-import Automaton ( iterateWorld, loadFromFile, saveToFile, name, Automaton, CellWorld (..), RuleCode )
+import Board (loadBoardFromFile)
+import Automaton ( CellWorld(CW), Automaton(code, name) )
 import Rules
+    ( supportedRulesDict, supportedRules, RuleImpl(RuleImpl) )
+import Worlds ( Worlds, WorldInstance(..), WorldIndex )
 
 import qualified Data.Map as Map
 import Data.Proxy (Proxy)
-import Control.Monad (unless)
+import Data.IORef ( IORef, newIORef, readIORef, writeIORef )
+import Control.Exception ( SomeException, try )
 
 
 -- iterateWorlds :: Int -> Worlds -> Worlds
@@ -21,49 +24,100 @@ import Control.Monad (unless)
 
 
 listRuleCodes :: IO ()
-listRuleCodes = mapM_ f supportedRules
+listRuleCodes = do
+  putStrLn "\nSupported rules ([code] name):"
+  mapM_ f supportedRules
   where
-    f (code, RuleImpl proxy) = putStrLn ("[" <> code <> "] " <> name proxy)
+    f (ruleCode, RuleImpl proxy) = putStrLn ("[" <> ruleCode <> "] " <> name proxy)
+
+listWorlds :: IORef Worlds -> IO ()
+listWorlds worldsRef = do
+  worlds <- readIORef worldsRef
+  putStrLn ("\nWorlds available: " <> show (Map.size worlds))
+  let ws = Map.toAscList worlds
+  mapM_ f ws
+  where
+    f :: (WorldIndex, WorldInstance) -> IO ()
+    f (idx, WorldInstance proxy _) = do
+      let strCode = code proxy
+      putStrLn (show idx <> ") [" <> strCode <> "]")
+
+loadWorld
+  :: forall rule        -- Brings `rule` into the scope of body
+   . Automaton rule     -- Demands the `rule` to be automaton.
+  => IORef Worlds
+  -> Proxy rule         -- Highlights what rule type was requrested by the caller.
+  -> FilePath
+  -> IO (Either String WorldIndex)
+loadWorld worldsRef proxy path = do
+  eBoard <- try (loadBoardFromFile path)
+  case eBoard of
+    Left (err :: SomeException) -> pure (Left (show err))
+    Right board -> do
+      worlds <- readIORef worldsRef
+
+      let idx = Map.size worlds
+      let w = WorldInstance @rule proxy (CW board)    -- specifying the automaton rule
+      let worlds' = Map.insert idx w worlds
+
+      writeIORef worldsRef worlds'
+      pure (Right idx)
 
 
-loadWorld :: RuleCode -> FilePath -> IO Bool
-loadWorld _ _ = error "Not implemented"
 
 
-processLoad :: IO ()
-processLoad = do
-  putStrLn "\nSupported rules to load ([code] name):"
-  listRuleCodes
-
+processLoad :: IORef Worlds -> IO ()
+processLoad worldsRef = do
   putStrLn "\nEnter rule code:"
   ruleCode <- getLine
+
   case Map.lookup ruleCode supportedRulesDict of
-    Nothing -> putStrLn "Unknown world type." >> go
+    Nothing -> putStrLn "Unknown world type." >> go worldsRef
     Just (RuleImpl proxy) -> do
       putStrLn "\nEnter world path:"
       path <- getLine
-      success <- loadWorld ruleCode path
-      unless success $ putStrLn ("\nFailed to load " <> ruleCode)
-      go
+
+      eIndex <- loadWorld worldsRef proxy path
+
+      case eIndex of
+        Left err  -> putStrLn ("Failed to load [" <> ruleCode <> "]: " <> err)
+        Right idx -> putStrLn ("Successfully loaded [" <> ruleCode <> "], index: " <> show idx)
+
+      go worldsRef
+
+printHelp :: IO ()
+printHelp = do
+  putStrLn "\nCommands:"
+  putStrLn "help   - this help message"
+  putStrLn "quit   - exit"
+  putStrLn "rules  - list supported rules"
+  putStrLn "load   - load a world"
+  putStrLn "worlds - list loaded worlds"
+
 
 
 main :: IO ()
 main = do
   putStrLn "Welcome to the world of cellular automata!"
-  go
+  printHelp
 
-go :: IO ()
-go = do
-  putStrLn "Type a command:"
+  worldsRef <- newIORef Map.empty
+  go worldsRef
+
+go :: IORef Worlds -> IO ()
+go worldsRef = do
+  putStrLn "\nType a command:"
   cmd <- getLine
 
   case cmd of
-    "load" -> processLoad
-    "q" -> pure ()
     "quit" -> pure ()
+    "help" -> printHelp >> go worldsRef
+    "rules" -> listRuleCodes >> go worldsRef
+    "worlds" -> listWorlds worldsRef >> go worldsRef
+    "load" -> processLoad worldsRef
     _ -> do
-      putStrLn "Unknown command."
-      go
+      putStrLn "Unknown command. Type `help` to see the list of commands."
+      go worldsRef
 
   -- gol1   :: GoL <- loadFromFile "./data/GoL/glider.txt"
   -- seeds1 :: Seeds <- loadFromFile "./data/Seeds/world1.txt"

aceeadfa3c216f22fd6a3a6332c5f11d536652c1
Alexander Granin <alexander@genlambda.com>
2023-09-14 16:16:26 +0700

    Parsing into existential KnownSymbol data type done.

--- First-Edition/BookSamples/CH03/Section3p1/test/ParsingGen4Spec.hs
+++ First-Edition/BookSamples/CH03/Section3p1/test/ParsingGen4Spec.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
 module ParsingGen4Spec where
 
 import Cell
@@ -38,27 +39,67 @@ appendCH :: CustomSymbol -> CustomChar -> CustomSymbol
 appendCH (CS chs) ch = CS (chs <> [ch])
 
 
-type MyParser = (Maybe CustomSymbol, String)
+type Parser = String -> Maybe (CustomSymbol, String)
 
+parseCustomChar :: String -> (Char, CustomChar) -> Maybe (CustomChar, String)
+parseCustomChar (a:rest) (a', ch) | a == a' = Just (ch, rest)
+parseCustomChar _ _ = Nothing
 
-parseB :: MyParser -> MyParser
-parseB (Nothing, rest) = (Nothing, rest)
-parseB (Just cs, 'B':rest) = (Just $ appendCH cs $ CH $ Proxy @"B", rest)
-parseB (mbCs, rest) = (mbCs, rest)
+parseB :: String -> Maybe (CustomSymbol, String)
+parseB str = let
+  mbParsed = parseCustomChar str ('B', CH (Proxy @"B"))
+  in case mbParsed of
+    Nothing -> Nothing
+    Just (ch, rest) -> Just (CS [ch], rest)
 
-parse1 :: MyParser -> MyParser
-parse1 (Nothing, rest) = (Nothing, rest)
-parse1 (Just cs, '1':rest) = (Just $ appendCH cs $ CH $ Proxy @"1", rest)
-parse1 (mbCs, rest) = (mbCs, rest)
+numbers :: Map.Map Char CustomChar
+numbers = Map.fromList
+  [ ('1', CH (Proxy @"1"))
+  , ('2', CH (Proxy @"2"))
+  , ('3', CH (Proxy @"3"))
+  , ('4', CH (Proxy @"4"))
+  , ('5', CH (Proxy @"5"))
+  , ('6', CH (Proxy @"6"))
+  , ('7', CH (Proxy @"7"))
+  , ('8', CH (Proxy @"8"))
+  , ('9', CH (Proxy @"9"))
+  ]
 
-parse2 :: String -> (Maybe (Proxy "2"), String)
-parse2 ('2':rest) = (Just $ Proxy @"2", rest)
-parse2 rest = (Nothing, rest)
+parseNumber :: String -> Maybe (CustomSymbol, String)
+parseNumber [] = Nothing
+parseNumber (a:rest) = case Map.lookup a numbers of
+  Nothing -> Nothing
+  Just ch -> Just (CS [ch], rest)
 
-parse3 :: String -> (Maybe (Proxy "3"), String)
-parse3 ('3':rest) = (Just $ Proxy @"3", rest)
-parse3 rest = (Nothing, rest)
+many :: Parser -> String -> Maybe (CustomSymbol, String)
+many p [] = Nothing
+many p str = case p str of
+  Nothing -> Nothing
+  Just (CS chs1, rest1) -> case many p rest1 of
+    Nothing -> Just (CS chs1, rest1)
+    Just (CS chs2, rest2) -> Just (CS (chs1 <> chs2), rest2)
 
+many1 :: Parser -> String -> Maybe (CustomSymbol, String)
+many1 p str = case p str of
+  Nothing -> error "Must be at least 1 symbol"
+  Just (CS chs1, rest1) ->
+    case many p rest1 of
+      Nothing -> Just (CS chs1, rest1)
+      Just (CS chs2, rest2) -> Just (CS (chs1 <> chs2), rest2)
+
+sequenceParsers :: [Parser] -> Parser
+sequenceParsers [] str = Nothing
+sequenceParsers (p:ps) str = case p str of
+  Nothing -> Nothing
+  Just (CS chs1, rest1) -> case sequenceParsers ps rest1 of
+    Nothing -> Just (CS chs1, rest1)
+    Just (CS chs2, rest2) -> Just (CS (chs1 <> chs2), rest2)
+
+myParsers :: [Parser]
+myParsers = [parseB, many1 parseNumber]
+
+parse :: Parser -> String -> Maybe (CustomSymbol, String)
+parse p = p
 
 mergePSymbols
   :: forall s1 s2
@@ -71,24 +112,34 @@ mergePSymbols _ _ = Proxy :: Proxy (AppendSymbol s1 s2)
 printPSymbol :: KnownSymbol s => Proxy (s :: Symbol) -> String
 printPSymbol = symbolVal
 
+
 spec :: Spec
 spec =
   describe "Parsing gen tests" $ do
     it "Test1" $ do
-      let (mbCs1, rest1) = parseB (Just (CS []), "B12")
-      let (mbCs2, rest2) = parse1 (mbCs1, rest1)
 
-      let (mbP1, rest1') = parse2 "23"
-      let (mbP2, rest2') = parse3 "3"
+      let mbParsed = parse (sequenceParsers myParsers) "B12"
 
-      let mbP12 = mergePSymbols <$> mbP1 <*> mbP2
+      case mbParsed of
+        Nothing -> print "not parsed"
+        Just (cs, rest) -> do
+          print $ showCS cs
+          print rest
 
-      print $ showCS <$> mbCs1
-      print $ showCS <$> mbCs2
-      print rest1
-      print rest2
+      -- let (mbCs1, rest1) = parseB (Just (CS []), "B12")
+      -- let (mbCs2, rest2) = parse1 (mbCs1, rest1)
 
-      print $ printPSymbol <$> mbP1
-      print $ printPSymbol <$> mbP2
-      print $ printPSymbol <$> mbP12
+      -- let (mbP1, rest1') = parseNumber (Just (CS []), "23")
+      -- let (mbP2, rest2') = parseNumber (mbP1, rest1')
+
+      -- let mbP12 = mergePSymbols <$> mbP1 <*> mbP2
+
+      -- print $ showCS <$> mbCs1
+      -- print $ showCS <$> mbCs2
+      -- print rest1
+      -- print rest2
+
+      -- print $ printPSymbol <$> mbP1
+      -- print $ printPSymbol <$> mbP2
+      -- print $ printPSymbol <$> mbP12
 

ffef02214dc8a53a9c08679ec6d082f319f87fa9
Alexander Granin <graninas@gmail.com>
2023-10-28 17:02:29 +0400

    Samples for Chapter 3.

--- First-Edition/BookSamples/CH03/Section1p2/src/Main.hs
+++ First-Edition/BookSamples/CH03/Section1p2/src/Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 
 module Main where
 
@@ -10,35 +11,47 @@ import GHC.TypeLits
 class Description a where
   describe :: Proxy a -> String
 
-
-data Benoit
-data Mandelbrot name
-
-type Fractal = Mandelbrot (Mandelbrot (Mandelbrot Benoit))
-
-instance Description Benoit where
-  describe _ = "Benoit"
-
-instance
-  Description n =>
-  Description (Mandelbrot n) where
-  describe _ = "Mandelbrot "
-    <> describe (Proxy @n)
-
-
 data Person (firstName :: Symbol) (lastName :: Symbol)
+data User (login :: Symbol) person
 
 instance
   (KnownSymbol fn, KnownSymbol ln) =>
   Description (Person fn ln) where
   describe _ =
-    symbolVal (Proxy @fn)
-    <> " "
-    <> symbolVal (Proxy @ln)
+    symbolVal (Proxy @fn) <> " " <> symbolVal (Proxy @ln)
+
+
+getUserDescription
+  :: forall login pincode person
+   . ( KnownSymbol login
+     , KnownNat pincode
+     , Description person
+     )
+  => Proxy (User login person)
+  -> String
+getUserDescription _
+     = "User: " <> symbolVal (Proxy @login)
+    <> ", pincode: " <> show (natVal (Proxy @pincode))
+    <> ", Person: " <> describe (Proxy @person)
+
+type MandelbrotUser = User "mandel" (Person "Benoit" "Mandelbrot")
+type InvalidUser = User "invalid" Int
 
 main :: IO ()
 main = do
-  print (describe (Proxy @Fractal))
   print (describe (Proxy @(Person "Benoit" "Mandelbrot")))
 
+  print (getUserDescription
+    @_
+    @4321
+    (Proxy @MandelbrotUser))
 
+
+
+type Hausdorff  = Person "Felix" "Hausdorff"
+type Mandelbrot = Person "Benoit" "Mandelbrot"
+
+data TLTip
+data TLList v next
+
+type Persons = TLList Mandelbrot (TLList Hausdorff TLTip)

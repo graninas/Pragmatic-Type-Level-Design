449c0aa84f17e487a74f3a332ad41a91a51d5389
Alexander Granin <graninas@gmail.com>
2024-05-29 22:17:01 +0400

    Pretty printing for static model fully works.

--- First-Edition/BookSamples/appendices/AppendixD_TypeLevelOOP/haskell_exist/src/TypeLevel/ZeplrogOOP/Static/Description.hs
+++ First-Edition/BookSamples/appendices/AppendixD_TypeLevelOOP/haskell_exist/src/TypeLevel/ZeplrogOOP/Static/Description.hs
@@ -6,17 +6,19 @@
 module TypeLevel.ZeplrogOOP.Static.Description where
 
 import CPrelude
+import qualified Prelude as P (unwords)
 
 import TypeLevel.ZeplrogOOP.Static.Model
 
 
 describe :: PropertyVL -> [String]
 describe prop = let
-  (_, ss) = execState (sPrint prop) (0, [])
+  (_, _, ss) = execState (sPrint prop) (0, False, [])
   in reverse ss
 
 type Indent = Int
-type Printer = State (Indent, [String]) ()
+type Raw = Bool
+type Printer = State (Indent, Raw, [String]) ()
 
 class SPrint item where
   sPrint :: item -> Printer
@@ -24,62 +26,83 @@ class SPrint item where
 
 push :: String -> Printer
 push line = do
-  (i, ss) <- get
+  (i, r, ss) <- get
   let line' = replicate (i * 2) ' ' <> line
-  put (i, line' : ss)
+  put (i, r, line' : ss)
+
+add
+  :: SPrint item
+  => item -> Printer
+add item = do
+  let (_, _, rawSS) = execState (sPrint item) (0, True, [])
+  let rawS = ' ' : P.unwords rawSS
+  (i, r, ss) <- get
+  case ss of
+    [] -> put (i, r, [rawS])
+    (s' : ss') -> put (i, r, (s' <> rawS) : ss')
+
+addS :: String -> Printer
+addS s = do
+  (i, r, ss) <- get
+  case ss of
+    [] -> put (i, r, [s])
+    (s' : ss') -> put (i, r, (s' <> s) : ss')
 
 indent :: Printer
 indent = do
-  (i, ss) <- get
-  put (i + 1, ss)
+  (i, r, ss) <- get
+  put (i + 1, r, ss)
 
 deIndent :: Printer
 deIndent = do
-  (i, ss) <- get
-  put (i - 1, ss)
+  (i, r, ss) <- get
+  put (i - 1, r, ss)
 
-
-subSPrint
+sub
   :: SPrint item
   => item -> Printer
-subSPrint item = do
-  indent
+sub item = do
+  (_, r, _) <- get
+  unless r indent
   sPrint item
-  deIndent
+  unless r deIndent
 
 instance SPrint PropertyVL where
   sPrint (StaticProp group) = do
-    push "Static property"
-    subSPrint group
+    push "StaticProp "
+    sub group
 
   sPrint (StaticPropRef prop) = do
-    push "Static property ref"
-    subSPrint prop
+    push "StaticPropRef"
+    sub prop
 
   sPrint (PropDict group kvs) = do
-    push "Property dict"
-    subSPrint group
-    subSPrint kvs
+    push "PropDict "
+    sub group
+    sub kvs
 
   sPrint (AbstractProp group kvs) = do
-    push "Abstract property"
-    subSPrint group
-    subSPrint kvs
+    push "AbstractProp "
+    sub group
+    sub kvs
 
   sPrint (DerivedProp ess prop kvs) = do
-    push "Derived property"
-    subSPrint ess
-    subSPrint prop
-    subSPrint kvs
+    push "DerivedProp "
+    add ess
+    sub prop
+    sub kvs
 
 instance SPrint PropertyGroupVL where
   sPrint (GroupId ess statPropId) = do
-    push "GroupId"
-    pure ()
+    push "GroupId "
+    add ess
+    add statPropId
 
   sPrint (GroupRootId ess statPropId prop) = do
-    push "Group root id"
-    pure ()
+    push "GroupRootId"
+    add ess
+    add statPropId
+    sub prop
 
   -- sPrint (GroupRoot _ _) = do
   --   push "Err: static group root can't be printed"
@@ -89,7 +112,61 @@ instance SPrint PropertyGroupVL where
 
 instance SPrint EssenceVL where
   sPrint (Ess ess) = do
-    push ("Ess: " <> ess)
+    push ("<" <> ess <> ">")
 
 instance SPrint [PropertyKeyValueVL] where
-  sPrint _ = pure ()
+  sPrint kvs = mapM_ sPrint kvs
+
+instance SPrint StaticPropertyId where
+  sPrint (StaticPropertyId i) = do
+    push ("(SPID: " <> show i)
+    addS ")"
+
+instance SPrint PropertyKeyValueVL where
+  sPrint (PropKeyBag ess owns) = do
+    push "PropKeyBag"
+    add ess
+    mapM_ sub owns
+
+  sPrint (PropKeyVal ess own) = do
+    push "PropKeyVal"
+    add ess
+    sub own
+
+instance SPrint PropertyOwningVL where
+  sPrint (OwnVal valDef) = do
+    push "OwnVal"
+    add valDef
+
+  sPrint (OwnProp prop) = do
+    push "OwnProp"
+    sub prop
+
+  sPrint (SharedProp prop) = do
+    push "SharedProp"
+    sub prop
+
+instance SPrint ValDefVL where
+  sPrint (IntValue i) = do
+    push ("(IntValue: " <> show i)
+    addS ")"
+
+  sPrint (BoolValue f) = do
+    push ("(BoolValue: " <> show f)
+    addS ")"
+
+  sPrint (PairValue p1 p2) = do
+    push "(PairValue: ("
+    add p1
+    add p2
+    addS ")"
+    addS ")"
+
+  sPrint (StringValue s) = do
+    push ("(StringValue: " <> s)
+    addS ")"
+
+  sPrint (PathValue esss) = do
+    push "(PathValue: "
+    mapM_ add esss
+    addS ")"

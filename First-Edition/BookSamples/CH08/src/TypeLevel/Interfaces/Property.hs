7404082f2729017fff8ee5064a34d2ea5346adb8
Alexander Granin <graninas@gmail.com>
2024-07-03 15:53:38 +0400

    Cleanup

--- First-Edition/BookSamples/CH08/src/TypeLevel/Interfaces/Property.hs
+++ First-Edition/BookSamples/CH08/src/TypeLevel/Interfaces/Property.hs
@@ -1,10 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeSynonymInstances #-}
-{-# LANGUAGE FlexibleInstances #-}
 
 module TypeLevel.Interfaces.Property where
 
@@ -28,14 +24,8 @@ type family MkAbstractProperty (a :: *) :: IAbstractProperty
 data IProperty
 type family MkProperty (a :: *) :: IProperty
 
-data IProperties
-type family MkProperties (a :: [IProperty]) :: IProperties
-
 data IField
 type family MkField (a :: *) :: IField
-
-data IFields
-type family MkFields (a :: [IField]) :: IFields
 
 data IPropertyOwning
 type family MkPropertyOwning (a :: *) :: IPropertyOwning
@@ -69,8 +59,8 @@ type OwnProp (prop :: IProperty) = MkPropertyOwning (OwnPropImpl prop)
 -- -- Property field implementation
 
 data KeyBagFieldImpl
-  (ess :: IEssence)
-  (props :: IProperties)
+  (ess   :: IEssence)
+  (props :: [IProperty])
 type KeyBagField ess props = MkField (KeyBagFieldImpl ess props)
 
 data KeyValFieldImpl
@@ -82,75 +72,19 @@ type KeyValField ess own = MkField (KeyValFieldImpl ess own)
 
 data AbstractPropertyImpl
   (pg :: IPropertyGroup)
-  (fs :: IFields)
+  (fs :: [IField])
 type AbstractProp pg fs = MkAbstractProperty (AbstractPropertyImpl pg fs)
 
 data AbstractDerivedPropImpl
-  (ess :: IEssence)
+  (ess   :: IEssence)
   (aProp :: IAbstractProperty)
-  (fs :: IFields)
+  (fs    :: [IField])
 type AbstractDerivedProp ess aProp fs = MkAbstractProperty (AbstractDerivedPropImpl ess aProp fs)
 
 -- -- Property implementation
+
 data DerivedPropImpl
-  (ess :: IEssence)
+  (ess   :: IEssence)
   (aProp :: IAbstractProperty)
-  (fs :: IFields)
+  (fs    :: [IField])
 type DerivedProp pg aProp fs = MkProperty (DerivedPropImpl pg aProp fs)
-
-type Fields fs = MkFields fs
-type Properties ps = MkProperties ps
-
--- data PropertyScript (lvl :: Level) where
---   PropScript
---     :: Essence lvl
---     -> CustomScript lvl
---     -> PropertyScript lvl
-
-
-
--- -- | Static property that must be stat and dyn materialized.
--- data Property (lvl :: Level) where
---   -- | Tag property reference.
---   TagPropRef
---     :: TagProperty lvl
---     -> Property lvl
-
---   -- | Derived property from any abstract property.
---   --    Will take the shape of the parent.
---   --    After static materialization, becomes PropDict.
---   --    DeriviedProp can't be value level.
---   DerivedProp
---     :: EssenceTL
---     -> AbstractProperty
---     -> [PropertyKeyValueTL]
---     -> [PropertyScriptTL]
---     -> PropertyTL
-
---   -- | Compound property for static value-level and dynamic
---   -- value-level representation.
---   -- Can't be type-level; use AbstractProp and DerivedProp instead.
---   PropDict
---     :: PropertyGroupVL
---     -> [PropertyKeyValueVL]
---     -> [PropertyScriptVL]
---     -> PropertyVL
-
-
--- ------ Short identifiers ----------
-
--- type PropertyGroupTL = PropertyGroup 'TypeLevel
--- type PropertyGroupVL = PropertyGroup 'ValueLevel
-
--- type PropertyScriptTL = PropertyScript 'TypeLevel
--- type PropertyScriptVL = PropertyScript 'ValueLevel
-
--- type PropertyTL = Property 'TypeLevel
--- type PropertyVL = Property 'ValueLevel
-
--- type PropertyKeyValueTL = PropertyKeyValue 'TypeLevel
--- type PropertyKeyValueVL = PropertyKeyValue 'ValueLevel
-
--- type PropertyOwningTL = PropertyOwning 'TypeLevel
--- type PropertyOwningVL = PropertyOwning 'ValueLevel
-
--- First-Edition/BookSamples/CH08/test/TypeLevel/InterfacesSpec.hs
+++ First-Edition/BookSamples/CH08/test/TypeLevel/InterfacesSpec.hs
@@ -1,13 +1,6 @@
 {-# LANGUAGE DataKinds                #-}
-{-# LANGUAGE TypeOperators            #-}
-{-# LANGUAGE FunctionalDependencies   #-}
-{-# LANGUAGE TypeFamilies             #-}
 {-# LANGUAGE PolyKinds                #-}
-{-# LANGUAGE KindSignatures           #-}
-{-# LANGUAGE FlexibleContexts         #-}
-{-# LANGUAGE FlexibleInstances        #-}
-{-# LANGUAGE ScopedTypeVariables      #-}
-{-# LANGUAGE TypeApplications         #-}
+{-# LANGUAGE TypeFamilies             #-}
 
 module TypeLevel.InterfacesSpec where
 
@@ -19,30 +12,6 @@ import GHC.TypeLits
 
 import Test.Hspec
 
--- type AbstractDoor = AbstractDerivedProp EAbstractDoor AnyProp
---   '[ PropKeyVal EIcon (OwnVal (IconVal "+"))
---    , PropKeyVal EHP   (OwnVal (HPTagVal 50))
---    , PropKeyVal EPos  (OwnVal (DerivablePosTagVal 0 0))
-
---     -- | Possible states
---    , PropKeyBag EStates
---       '[ TagPropRef StateOpen
---        , TagPropRef StateClose
---        ]
-
---     -- | Current state. Points to a close/open state
---    , PropKeyVal EState (OwnVal (PathValue CloseStateRef))
---    ]
---    '[PropScript EOpenDoorScript OpenDoorScript]
-
-
--- -- | Specific door with a specific icon.
--- type SpecificDoor = DerivedProp ESpecificDoor AbstractDoor
---   '[ PropKeyVal EIcon (OwnVal (IconVal "?"))
---    , PropKeyVal EHP   (OwnVal (HPTagVal 50))
---    , PropKeyVal EPos  (OwnVal (PosTagVal 2 3))    -- overridden
---    ]
---    '[]
 
 type EAbstractDoor   = Essence "object:abstract door"
 type ESpecificDoor   = Essence "object:specific door"
@@ -55,81 +24,19 @@ type HPTagVal (n :: Nat) = Dummy
 type PosTagVal (x :: Nat) (y :: Nat) = Dummy
 
 type AbstractDoor = AbstractProp (Group EAbstractDoor)
-  ( Fields
-      '[ KeyValField EIcon (OwnVal (IconVal "+"))
+  '[ KeyValField EIcon (OwnVal (IconVal "+"))
        , KeyValField EHP   (OwnVal (HPTagVal 50))
        , KeyValField EPos  (OwnVal (PosTagVal 0 0))
-       ]
-  )
+   ]
 
 type SpecificDoor = DerivedProp ESpecificDoor AbstractDoor
-  ( Fields
-      '[ KeyValField EIcon (OwnVal (IconVal "?"))
-       , KeyValField EHP   (OwnVal (HPTagVal 50))
-       , KeyValField EPos  (OwnVal (PosTagVal 2 3))
-       ]
-  )
-
-
-
--- type UKOnly  = Censorship (ExtL.AllowedCountries "UK only" '[UK])
--- type UKAndUS = Censorship (ExtL.AllowedCountries "UK & US" '[UK, US])
-
--- type MinBid202 = 'ValNameS "202 min bid"
--- type PayloadLot1 = LotPayload (ExtL.EFLotPayload (MoneyVal "1000"))
--- type PayloadLot2 = LotPayload (ExtL.EFLotPayload (MoneyDynVal MinBid202))
--- type PayloadLot3 = LotPayload (ExtL.EFLotPayload (MoneyVal "40000"))
-
--- -- Auction algorithm
-
--- type EnglishAuctionLotAction1 =
---   ( Action (GetLotName (ConcatL "New lot: " Print))
---   ( Action (GetLotDescr Print)
---     End
---   ))
-
--- type EnglishAuctionLotAction2 =
---   ( Action (GetLotName2  (ConcatL "New lot: "         (Both (WriteRef "LotName result" String)  Print)))
---   ( Action (GetLotDescr2 (ConcatL "Lot description: " (Both (WriteRef "LotDescr result" String) Print)))
---     End
---   ))
-
--- type EnglishAuctionFlow = AuctionFlow
---   ( LotProcess EnglishAuctionLotAction1
---   )
-
-
--- type TestFlow = AuctionFlow
---   ( LotProcess
---       ( Action (ReadRef "curRound" Int Print)
---         ( Action (ReadRef "curCost" Int Drop)
---           End
---         )
---       )
---   )
-
--- -- Auction
-
--- type WorldArtsInfo = Info "World arts" "UK Bank"
--- type WorldArtsLots = Lots
---   '[ Lot "101" "Dali artwork"      PayloadLot1 (Currency ExtL.GBP) UKOnly
---    , Lot "202" "Chinese vase"      PayloadLot2 (Currency ExtL.USD) UKAndUS
---    , Lot "303" "Ancient mechanism" PayloadLot3 (Currency ExtL.USD) NoCensorship
---    ]
-
--- type WorldArtsAuction = Auction
---   EnglishAuctionFlow
---   WorldArtsInfo
---   WorldArtsLots
-
--- type TestAuction = Auction
---   TestFlow
---   WorldArtsInfo
---   WorldArtsLots
+  '[ KeyValField EIcon (OwnVal (IconVal "?"))
+   , KeyValField EHP   (OwnVal (HPTagVal 50))
+   , KeyValField EPos  (OwnVal (PosTagVal 2 3))
+   ]
 
 spec :: Spec
 spec = do
   describe "Type level interfaces" $ do
-
     xit "Property materialization test" $ do
       1 `shouldBe` 2

06625c3831e4907b6710bba400a59b234952bcc5
Alexander Granin <graninas@gmail.com>
2024-10-07 22:30:39 +0400

    Newtype example. Small fixes

--- First-Edition/BookSamples/rosetta-stone/rust/rs00_newtype/src/main.rs
+++ First-Edition/BookSamples/rosetta-stone/rust/rs00_newtype/src/main.rs
@@ -1,10 +1,22 @@
 
-struct FirstName(String);
-struct LastName(String);
+#[allow(dead_code)]
+struct FirstName(&'static str);
+#[allow(dead_code)]
+struct LastName(&'static str);
 
+#[allow(dead_code)]
+const PERSON_FIRST_NAME: FirstName = FirstName("Jack");
+const PERSON_LAST_NAME: LastName = LastName("O'Neill");
 
+#[allow(dead_code)]
+struct FilePath(&'static str);
+const LAST_FILE_NAME: FilePath = FilePath("last_file_name.txt");
 
+#[allow(unused_variables)]
+fn write_last_name(path: FilePath, name: LastName) {
+  // do something
+}
 
 fn main() {
-  println!("{}", strs);
+  write_last_name(LAST_FILE_NAME, PERSON_LAST_NAME);
 }

d41a9653ca075b3b14f442b4a1e2a97e6eec748c
Alexander Granin <graninas@gmail.com>
2024-11-04 13:32:21 +0400

    Cleanup

--- First-Edition/BookSamples/rosetta-stone/scala3/rsc2p1p1_newtype/src/main/scala/Main.scala
+++ First-Edition/BookSamples/rosetta-stone/scala3/rsc2p1p1_newtype/src/main/scala/Main.scala
@@ -1,30 +1,4 @@
 
-// #[allow(dead_code)]
-// struct FirstName(&'static str);
-// #[allow(dead_code)]
-// struct LastName(&'static str);
-
-// #[allow(dead_code)]
-// const PERSON_FIRST_NAME: FirstName = FirstName("Jack");
-// const PERSON_LAST_NAME: LastName = LastName("O'Neill");
-
-// #[allow(dead_code)]
-// struct FilePath(&'static str);
-// const LAST_FILE_NAME: FilePath = FilePath("last_file_name.txt");
-
-// #[allow(unused_variables)]
-// fn write_last_name(path: FilePath, name: LastName) {
-//   // do something
-// }
-
-// fn main() {
-//   write_last_name(LAST_FILE_NAME, PERSON_LAST_NAME);
-// }
-
-
-// opaque type FirstName = String
-// opaque type LastName = String
-
 class FirstName(protected val name: String):
   override def toString(): String = name
 

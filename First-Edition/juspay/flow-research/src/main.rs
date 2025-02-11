df0a0ce37e0d4bd2a9c6288bee99c1b50fe33474
Alexander Granin <graninas@gmail.com>
2025-02-11 17:17:55 +0700

    Step and variable declaration demo done.

--- First-Edition/juspay/flow-research/src/main.rs
+++ First-Edition/juspay/flow-research/src/main.rs
@@ -24,8 +24,22 @@ pub struct IStory;
 pub struct IScenario;
 pub struct ICondition;
 pub struct IParam;
-pub struct IValue<TypeTag: IInterface<ITypeTag>>
+pub struct IVar<TypeTag: IInterface<ITypeTag>>
   (PhantomData<TypeTag>);
+pub struct IVal<TypeTag: IInterface<ITypeTag>>
+  (PhantomData<TypeTag>);
+pub struct IStep;
+pub struct IAction;
+
+pub struct VarImpl<const IDX: u8>;
+
+
+pub struct NoCondImpl;
+pub type NoCond = Wrapper<ICondition, NoCondImpl>;
+
+pub struct GreaterImpl<const N: i32>;
+pub type Greater<const N: i32> = Wrapper<ICondition, GreaterImpl<N>>;
+
 
 pub struct StoryImpl<
     Name: IInterface<IName>,
@@ -38,48 +52,68 @@ pub type Story<Name, Scenarios> =
 
 pub struct ScenarioImpl<
     Name: IInterface<IName>,
-    Params: HList<IParam>>
-    (PhantomData<(Name, Params)>);
+    Params: HList<IParam>,
+    Steps: HList<IStep>>
+    (PhantomData<(Name, Params, Steps)>);
 
-  pub type Scenario<Name, Params> =
-  Wrapper<IScenario, ScenarioImpl<Name, Params>>;
-
-
-pub struct NoCondImpl;
-pub type NoCond = Wrapper<ICondition, NoCondImpl>;
+pub type Scenario<Name, Params, Steps> =
+    Wrapper<IScenario, ScenarioImpl<Name, Params, Steps>>;
 
 
 pub struct MandatoryParamImpl<
     PName: IInterface<IName>,
     PType: IInterface<ITypeTag>,
+    Var: IInterface<IVar<PType>>,
     Condition: IInterface<ICondition>>
-    (PhantomData<(PName, PType, Condition)>);
+    (PhantomData<(PName, PType, Var, Condition)>);
 
-pub type MandatoryParam<PName, PType, Condition> =
-    Wrapper<IParam, MandatoryParamImpl<PName, PType, Condition>>;
+pub type MandatoryParam<PName, PType, Var, Condition> =
+    Wrapper<IParam, MandatoryParamImpl<PName, PType, Var, Condition>>;
 
 pub struct OptionalParamImpl<
     PName: IInterface<IName>,
     PType: IInterface<ITypeTag>,
-    Default: IInterface<IValue<PType>>,
-    Condition: IInterface<ICondition>>
-    (PhantomData<(PName, PType, Default, Condition)>);
+    Var: IInterface<IVar<PType>>,
+    Default: IInterface<IVal<PType>>>
+    (PhantomData<(PName, PType, Var, Default)>);
 
-pub type OptionalParam<PName, PType, Default, Condition> =
-    Wrapper<IParam, OptionalParamImpl<PName, PType, Default, Condition>>;
+pub type OptionalParam<PName, PType, Var, Default> =
+    Wrapper<IParam, OptionalParamImpl<PName, PType, Var, Default>>;
 
+pub struct MandatoryAuthImpl<
+    PName: IInterface<IName>,
+    PType: IInterface<ITypeTag>,
+    Var: IInterface<IVar<PType>>>
+    (PhantomData<(PName, PType, Var)>);
 
+pub type MandatoryAuth<PName, PType, Var> =
+    Wrapper<IParam, MandatoryAuthImpl<PName, PType, Var>>;
 
-mod names {
-    use tl_interface::Wrapper;
-    use crate::{*};
+pub struct StepImpl<
+    Action: IInterface<IAction>,
+    Out: IInterface<IName>>
+    (PhantomData<(Action, Out)>);
 
-    pub struct PaymentsTag;
-    pub struct NormalPaymentTag;
+pub type Step<Action, Out> =
+    Wrapper<IStep, StepImpl<Action, Out>>;
 
-    pub type Payments = Wrapper<IName, PaymentsTag>;
-    pub type NormalPayment = Wrapper<IName, NormalPaymentTag>;
-}
+pub struct ConditionalStepImpl<
+    Condition: IInterface<ICondition>,
+    TrueSteps: HList<IStep>,
+    FalseSteps: HList<IStep>>
+    (PhantomData<(Condition, TrueSteps, FalseSteps)>);
+
+pub type ConditionalStep<Condition, TrueSteps, FalseSteps> =
+    Wrapper<IStep, ConditionalStepImpl<Condition, TrueSteps, FalseSteps>>;
+
+// This step stops the scenario execution if the condition is not met.
+pub struct GuardStepImpl<
+    Condition: IInterface<ICondition>,
+    Steps: HList<IStep>>
+    (PhantomData<(Condition, Steps)>);
+
+pub type GuardStep<Condition, Steps> =
+    Wrapper<IStep, GuardStepImpl<Condition, Steps>>;
 
 // Common types
 
@@ -91,42 +125,98 @@ pub type IntType = Wrapper<ITypeTag, IntImpl>;
 
 // Payment-specific types
 
-pub struct AmountImpl;
-pub type AmountType = Wrapper<ITypeTag, AmountImpl>;
+pub struct AmountTypeImpl;
+pub type AmountType = Wrapper<ITypeTag, AmountTypeImpl>;
 
-pub struct CurrencyImpl;
-pub type CurrencyType = Wrapper<ITypeTag, CurrencyImpl>;
+pub struct CurrencyTypeImpl;
+pub type CurrencyType = Wrapper<ITypeTag, CurrencyTypeImpl>;
 
-pub struct PaymentMethodImpl;
-pub type PaymentMethodType = Wrapper<ITypeTag, PaymentMethodImpl>;
+pub struct PaymentMethodTypeImpl;
+pub type PaymentMethodType = Wrapper<ITypeTag, PaymentMethodTypeImpl>;
 
-pub struct ConfirmationImpl;
-pub type ConfirmationType = Wrapper<ITypeTag, ConfirmationImpl>;
+pub struct ConfirmationTypeImpl;
+pub type ConfirmationType = Wrapper<ITypeTag, ConfirmationTypeImpl>;
 
-pub struct CaptureMethodImpl;
-pub type CaptureMethodType = Wrapper<ITypeTag, CaptureMethodImpl>;
+pub struct CaptureMethodTypeImpl;
+pub type CaptureMethodType = Wrapper<ITypeTag, CaptureMethodTypeImpl>;
 
-mod params {
+pub struct MerchantApiKeyTypeImpl;
+pub type MerchantApiKeyType = Wrapper<ITypeTag, MerchantApiKeyTypeImpl>;
+
+pub struct MerchantProfileTypeImpl;
+pub type MerchantProfileType = Wrapper<ITypeTag, MerchantProfileTypeImpl>;
+
+pub struct PaymentGatewayTypeImpl;
+pub type PaymentGatewayType = Wrapper<ITypeTag, PaymentGatewayTypeImpl>;
+
+pub struct PaymentIntentTypeImpl;
+pub type PaymentIntentType = Wrapper<ITypeTag, PaymentIntentTypeImpl>;
+
+pub struct PaymentTypeImpl;
+pub type PaymentType = Wrapper<ITypeTag, PaymentTypeImpl>;
+
+pub struct PaymentStatusTypeImpl;
+pub type PaymentStatusType = Wrapper<ITypeTag, PaymentStatusTypeImpl>;
+
+mod names {
     use tl_interface::Wrapper;
     use crate::{*};
 
-    pub struct AmountTag;
-    pub type Amount = Wrapper<IName, AmountTag>;
+    pub struct PaymentsNameTag;
+    pub type Payments = Wrapper<IName, PaymentsNameTag>;
 
-    pub struct CurrencyTag;
-    pub type Currency = Wrapper<IName, CurrencyTag>;
+    pub struct NormalPaymentNameTag;
+    pub type NormalPayment = Wrapper<IName, NormalPaymentNameTag>;
 
-    pub struct DescriptionTag;
-    pub type Description = Wrapper<IName, DescriptionTag>;
+    // These names correspond to the names in the JSON payload.
+    // The structs replace tl_str!() macro to keep types nesting limit lower.
+    pub struct AmountNameTag;
+    pub type Amount = Wrapper<IName, AmountNameTag>;
 
-    pub struct PaymentMethodTag;
-    pub type PaymentMethod = Wrapper<IName, PaymentMethodTag>;
+    // Alternatively, it can be:
+    // pub type Amount = tl_str!("amount");
+    // Or:
+    // pub type Amount = Wrapper<IName, tl_str!("amount")>;
 
-    pub struct ConfirmationTag;
-    pub type Confirmation = Wrapper<IName, ConfirmationTag>;
+    pub struct CurrencyNameTag;
+    pub type Currency = Wrapper<IName, CurrencyNameTag>;
 
-    pub struct CaptureMethodTag;
-    pub type CaptureMethod = Wrapper<IName, CaptureMethodTag>;
+    pub struct DescriptionNameTag;
+    pub type Description = Wrapper<IName, DescriptionNameTag>;
+
+    pub struct PaymentMethodNameTag;
+    pub type PaymentMethod = Wrapper<IName, PaymentMethodNameTag>;
+
+    pub struct ConfirmationNameTag;
+    pub type Confirmation = Wrapper<IName, ConfirmationNameTag>;
+
+    pub struct CaptureMethodNameTag;
+    pub type CaptureMethod = Wrapper<IName, CaptureMethodNameTag>;
+
+    pub struct MerchantApiKeyNameTag;
+    pub type MerchantApiKey = Wrapper<IName, MerchantApiKeyNameTag>;
+}
+
+mod vars {
+    use tl_interface::Wrapper;
+    use crate::{*};
+
+  // Variable types. These are used to store values during the scenario execution.
+  // Indexes are used to distinguish between different variables of the same type.
+  // Indexes should not be reused within the same scenario.
+    pub type Amount<const IDX: u8>         = Wrapper<IVar<AmountType>, VarImpl<IDX>>;
+    pub type Currency<const IDX: u8>       = Wrapper<IVar<CurrencyType>, VarImpl<IDX>>;
+    pub type Str<const IDX: u8>            = Wrapper<IVar<StringType>, VarImpl<IDX>>;
+    pub type PaymentMethod<const IDX: u8>  = Wrapper<IVar<PaymentMethodType>, VarImpl<IDX>>;
+    pub type Confirmation<const IDX: u8>   = Wrapper<IVar<ConfirmationType>, VarImpl<IDX>>;
+    pub type CaptureMethod<const IDX: u8>  = Wrapper<IVar<CaptureMethodType>, VarImpl<IDX>>;
+    pub type MerchantApiKey<const IDX: u8> = Wrapper<IVar<MerchantApiKeyType>, VarImpl<IDX>>;
+
+    pub type MerchantProfile<const IDX: u8> = Wrapper<IVar<MerchantProfileType>, VarImpl<IDX>>;
+    pub type PaymentGateway<const IDX: u8>  = Wrapper<IVar<PaymentGatewayType>, VarImpl<IDX>>;
+    pub type PaymentIntent<const IDX: u8>   = Wrapper<IVar<PaymentIntentType>, VarImpl<IDX>>;
+    pub type Payment<const IDX: u8>         = Wrapper<IVar<PaymentType>, VarImpl<IDX>>;
+    pub type PaymentStatus<const IDX: u8>   = Wrapper<IVar<PaymentStatusType>, VarImpl<IDX>>;
 }
 
 mod vals {
@@ -134,7 +224,7 @@ mod vals {
     use crate::{*};
 
     pub struct EmptyStringTag;
-    pub type EmptyString = Wrapper<IValue<StringType>, EmptyStringTag>;
+    pub type EmptyString = Wrapper<IVal<StringType>, EmptyStringTag>;
 }
 
 mod confirmation {
@@ -142,10 +232,10 @@ mod confirmation {
     use crate::{*};
 
     pub struct ManualTag;
-    pub type Manual = Wrapper<IValue<ConfirmationType>, ManualTag>;
+    pub type Manual = Wrapper<IVal<ConfirmationType>, ManualTag>;
 
     pub struct AutomaticTag;
-    pub type Automatic = Wrapper<IValue<ConfirmationType>, AutomaticTag>;
+    pub type Automatic = Wrapper<IVal<ConfirmationType>, AutomaticTag>;
 }
 
 mod capture_method {
@@ -153,10 +243,10 @@ mod capture_method {
     use crate::{*};
 
     pub struct ManualTag;
-    pub type Manual = Wrapper<IValue<CaptureMethodType>, ManualTag>;
+    pub type Manual = Wrapper<IVal<CaptureMethodType>, ManualTag>;
 
     pub struct AutomaticTag;
-    pub type Automatic = Wrapper<IValue<CaptureMethodType>, AutomaticTag>;
+    pub type Automatic = Wrapper<IVal<CaptureMethodType>, AutomaticTag>;
 }
 
 
@@ -169,10 +259,10 @@ mod payment_methods {
     pub struct IPaymentMethod;
 
     pub struct CardImpl<
-        Number: IInterface<IValue<StringType>>,
-        ExpMonth: IInterface<IValue<IntType>>,
-        ExpYear: IInterface<IValue<IntType>>,
-        Cvv: IInterface<IValue<StringType>>
+        Number: IInterface<IVal<StringType>>,
+        ExpMonth: IInterface<IVal<IntType>>,
+        ExpYear: IInterface<IVal<IntType>>,
+        Cvv: IInterface<IVal<StringType>>
         >
     (PhantomData<(Number, ExpMonth, ExpYear, Cvv)>);
     pub type Card<Number, ExpMonth, ExpYear, Cvv> =
@@ -195,24 +285,152 @@ pub type PaymentsStory = Story<
         NormalPaymentScenario]>;
 
 
-pub struct InstantPaymentMethodsCond;
-pub type InstantPaymentMethods = Wrapper<ICondition, InstantPaymentMethodsCond>;
+pub struct IsInstantPaymentMethodCond <P: IInterface<IVar<PaymentMethodType>>>
+  (PhantomData<P>);
+pub type IsInstantPaymentMethod<P>
+  = Wrapper<ICondition, IsInstantPaymentMethodCond<P>>;
 
 
 pub type NormalPaymentScenario = Scenario<
     names::NormalPayment,
     tl_list![
         IParam,
-        MandatoryParam<params::Amount, AmountType, NoCond>,
-        MandatoryParam<params::Currency, CurrencyType, NoCond>,
-        MandatoryParam<params::PaymentMethod, PaymentMethodType, InstantPaymentMethods>,
-        OptionalParam<params::Description, StringType, vals::EmptyString, NoCond>,
-        OptionalParam<params::Confirmation, ConfirmationType, confirmation::Manual, NoCond>,
-        OptionalParam<params::CaptureMethod, CaptureMethodType, capture_method::Manual, NoCond>
-        ]
-    >;
+        MandatoryParam<names::Amount,         AmountType,        vars::Amount<1>,        Greater<0>>,
+        MandatoryParam<names::Currency,       CurrencyType,      vars::Currency<1>,      NoCond>,
+        MandatoryParam<names::PaymentMethod,  PaymentMethodType, vars::PaymentMethod<1>, NoCond>,
+        OptionalParam <names::Description,    StringType,        vars::Str<1>,           vals::EmptyString>,
+        OptionalParam <names::Confirmation,   ConfirmationType,  vars::Confirmation<1>,  confirmation::Manual>,
+        OptionalParam <names::CaptureMethod,  CaptureMethodType, vars::CaptureMethod<1>, capture_method::Manual>,
+        MandatoryAuth <names::MerchantApiKey, StringType,        vars::MerchantApiKey<1>>
+        ],
 
+    GuardStep<
+      IsInstantPaymentMethod<vars::PaymentMethod<1>>,
+      tl_list![IStep,
+        Step<LoadMerchantProfile<vars::MerchantApiKey<1>>, vars::MerchantProfile<1>>,
+        Step<ChoosePaymentGateway<vars::MerchantProfile<1>>, vars::PaymentGateway<1>>,
+        ConditionalStep<
+          PaymentIntentRequired<vars::PaymentGateway<1>>,
+          tl_list![IStep,
+            Step<CreatePaymentIntent<vars::PaymentGateway<1>>, vars::PaymentIntent<1>>,
+            Step<AttachPaymentMethod<vars::PaymentGateway<1>, vars::PaymentIntent<1>>, vars::Payment<1>>,
+            Step<AuthorizePayment<vars::PaymentGateway<1>, vars::Payment<1>>, vars::PaymentStatus<1>>
+          ],
+          tl_list![IStep,
+            Step<CreatePayment<vars::PaymentGateway<1>>, vars::Payment<1>>,
+            Step<AuthorizePayment<vars::PaymentGateway<1>, vars::Payment<1>>, vars::PaymentStatus<1>>
+          ]
+        >
+      ]
+    >
 
+  >;
+
+pub struct LoadMerchantProfileImpl<
+  ApiKey: IInterface<IVar<MerchantApiKeyType>>
+  >(PhantomData<ApiKey>);
+pub type LoadMerchantProfile<ApiKey> =
+  Wrapper<IAction, LoadMerchantProfileImpl<ApiKey>>;
+
+pub struct ChoosePaymentGatewayImpl<
+  Profile: IInterface<IVar<MerchantProfileType>>
+  >(PhantomData<Profile>);
+pub type ChoosePaymentGateway<Profile> =
+  Wrapper<IAction, ChoosePaymentGatewayImpl<Profile>>;
+
+pub struct PaymentIntentRequiredImpl<
+  Gateway: IInterface<IVar<PaymentGatewayType>>
+  >(PhantomData<Gateway>);
+
+pub type PaymentIntentRequired<Gateway> =
+  Wrapper<ICondition, PaymentIntentRequiredImpl<Gateway>>;
+
+pub struct CreatePaymentIntentImpl<
+  Gateway: IInterface<IVar<PaymentGatewayType>>
+  >(PhantomData<Gateway>);
+
+pub type CreatePaymentIntent<Gateway> =
+  Wrapper<IAction, CreatePaymentIntentImpl<Gateway>>;
+
+pub struct AttachPaymentMethodImpl<
+  Gateway: IInterface<IVar<PaymentGatewayType>>,
+  Payment: IInterface<IVar<PaymentIntentType>>
+  >(PhantomData<(Gateway, Payment)>);
+
+pub type AttachPaymentMethod<Gateway, Payment> =
+  Wrapper<IAction, AttachPaymentMethodImpl<Gateway, Payment>>;
+
+pub struct AuthorizePaymentImpl<
+  Gateway: IInterface<IVar<PaymentGatewayType>>,
+  Payment: IInterface<IVar<PaymentType>>
+  >(PhantomData<(Gateway, Payment)>);
+
+pub type AuthorizePayment<Gateway, Payment> =
+  Wrapper<IAction, AuthorizePaymentImpl<Gateway, Payment>>;
+
+pub struct CreatePaymentImpl<
+  Gateway: IInterface<IVar<PaymentGatewayType>>
+  >(PhantomData<Gateway>);
+
+pub type CreatePayment<Gateway> =
+  Wrapper<IAction, CreatePaymentImpl<Gateway>>;
+
+    // Pre-conditions:
+//   a. Client → Merchant: "I want to buy this item. Here’s my <payment method>."
+//   b. Merchant → Orchestrator: Sends the payment request.
+// Success scenario:
+//   1. Orchestrator: Loads the merchant profile.
+//        In: <merchant_api_key>
+//        Out: <merchant_profile>
+//   2. Orchestrator: Determines the best Payment Gateway based on
+//        the merchant’s preferences, customer eligibility, and availability.
+//      Conditions:
+//        - Chosen Payment Gateway has payment confirm and capture procedures.
+//        In: <merchant_profile>
+//        Out: <chosen_payment_gateway>
+//   3. Orchestrator → <chosen_payment_gateway>: We need to create a payment.
+//     3a. Payment Gateway requires payment intent procedure.
+//               Orchestrator → <chosen_payment_gateway>: Create payment intent without payment method.
+//               Orchestrator → <chosen_payment_gateway>: Attach <payment_method>.
+//               Orchestrator → <chosen_payment_gateway>: Authorize payment.
+//               <chosen_payment_gateway>:
+//                 - Requests payment authorization in the bank. Gets approval and authorization code.
+//                 - Returns the payment status (payment is authorized).
+//     3b. Payment Gateway doesn't handle payment intent procedure.
+//               Orchestrator → <chosen_payment_gateway>: Create payment with <payment_method>.
+//               Orchestrator → <chosen_payment_gateway>: Authorize payment.
+//               <chosen_payment_gateway>:
+//                 - Requests payment authorization in the bank. Gets approval and authorization code.
+//                 - Returns the payment status (payment is authorized).
+//   4. Confirmation.
+//     4a. Confirmation is Automatic. Same session.
+//         Orchestrator → <chosen_payment_gateway>: Confirm payment.
+//             <chosen_payment_gateway>:
+//               - Confirms the payment in the bank.
+//               - Returns the payment status (payment is confirmed).
+//     4b. Confirmation is Manual. Separate session.
+//         Orchestrator: Waits for the confirmation.
+//         Merchant → Orchestrator (After a while): Confirm the payment.
+//         Orchestrator → <chosen_payment_gateway>: Confirm payment.
+//             <chosen_payment_gateway>:
+//               - Confirms the payment in the bank.
+//               - Returns the payment status (payment is confirmed).
+//   5. Capture.
+//     5a. Capture is Automatic. Same session.
+//         Orchestrator → <chosen_payment_gateway>: Capture payment.
+//             <chosen_payment_gateway>:
+//               - Captures the payment in the bank.
+//               - Returns the payment status (payment is captured).
+//     5b. Capture is Manual. Separate session.
+//         Orchestrator: Waits for the capture.
+//         Merchant → Orchestrator (After a while): Capture the payment.
+//         Orchestrator → <chosen_payment_gateway>: Capture payment.
+//             <chosen_payment_gateway>:
+//               - Captures the payment in the bank.
+//               - Returns the payment status (payment is captured).
+//   6. Orchestrator → Merchant: Sends the payment status (payment is captured).
+// Post-conditions:
+//   a. Merchant → Client: Notifies the payment is successful.
 
 
 
